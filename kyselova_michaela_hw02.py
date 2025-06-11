8dc5587dcc05b65dbd25a49a62b2a3cef7729ed4
mikyselova <mikyselova@seznam.cz>
2025-06-11 23:49:04 +0200

    Commitnutí opravy

--- kyselova_michaela_hw02.py
+++ kyselova_michaela_hw02.py
@@ -2,42 +2,28 @@ import csv
 import json
 
 with open("netflix_titles.tsv", mode="r", encoding="utf-8") as file:
-    reader = csv.DictReader(file, delimiter="\t")
-    data = list(reader)
-
-new_data = []
-wanted_keys = ["PRIMARYTITLE", "DIRECTOR", "CAST", "GENRES", "STARTYEAR"]
+    data = list(csv.DictReader(file, delimiter="\t"))
+    
+output = []
 
 for row in data:
-    new_row = {}
-    for key in wanted_keys:
-        if key in row:
-            new_row[key] = row[key]
-    new_data.append(new_row)
+    movie = {}
 
-rename = {
-    "PRIMARYTITLE": "title",
-    "DIRECTOR": "directors",
-    "CAST": "cast",
-    "GENRES": "genres",
-    "STARTYEAR": "decade"
-}
+    movie["title"] = row["PRIMARYTITLE"].strip()
 
-for row in new_data:
-    for old_key, new_key in rename.items():
-        if old_key in row:
-            row[new_key] = row.pop(old_key)
-    
-    for key, delimiter in [
-            ("directors", ", "), ("cast", ", "), ("genres", ",")]:
-        value = row.get(key)
-        if value:
-            row[key] = value.split(delimiter)
-        else:
-            row[key] = []
-    
-    year = int(row["decade"])
-    row["decade"] = year - (year % 10)
+    director = row["DIRECTOR"].strip()
+    movie["directors"] = [d.strip() for d in director.split(",")] if director else []
 
+    cast = row["CAST"].strip()
+    movie["cast"] = [c.strip() for c in cast.split(",")] if cast else []
+
+    genres = row["GENRES"].strip()
+    movie["genres"] = [g.strip() for g in genres.split(",")]
+
+    year = row["STARTYEAR"].strip()
+    if year.isdigit():
+        movie["decade"] = int(year) - int(year) % 10
+        output.append(movie)
+        
 with open("netflix_titles.json", mode="w", encoding="utf-8") as json_file:
-    json.dump(new_data, json_file, ensure_ascii=False, indent=4)
+    json.dump(output, json_file, ensure_ascii=False, indent=4)
